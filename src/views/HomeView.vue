616a37c8df06bf69e117f1e4ae9cea9b044265c8
Osunla Blessing Temitope <osunlab710@gmail.com>
2023-09-05 10:10:13 +0100

    .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
   <main>
     <FirstPage />
-    <!-- <RouterLink to="/signupForm">Go To Signup Form</RouterLink>
-    <RouterLink to="/loginForm">Go To Login Form</RouterLink>
+    <RouterLink to="/signupForm">Go To Signup Form</RouterLink>
+    <!-- <RouterLink to="/loginForm">Go To Login Form</RouterLink>
     <RouterLink to="/dashBoard">Go To Dashboard</RouterLink> -->
   </main>
 </template>
